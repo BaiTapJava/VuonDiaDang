2f77ba898839dd27eb10860c6812fcf8c0b8fb25
ThinhLuffy <41873503+ThinhLuffy@users.noreply.github.com>
2018-10-05 17:54:45 +0700

    Vuon Dia Dang

---
+++ vuonDiaDang/src/Apple.java
@@ -0,0 +1,10 @@
+public class Apple {
+    public int weight = 10;
+
+    public boolean isEmpty(){
+        if(this.weight <= 0)
+            return true;
+        else
+            return false;
+    }
+}
